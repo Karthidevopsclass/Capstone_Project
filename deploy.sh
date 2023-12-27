bfbe7df3fcba5d100b4b685fc6fdae2f17c7ea8c
root <root@ip-172-31-1-215.ap-south-1.compute.internal>
2023-12-27 09:13:53 +0000

    Commit

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
-ssh -o StrictHostKeyChecking=no -i /root/.ssh/newpem.pem ec2-user@13.234.77.141 <<EOF
+ssh -o StrictHostKeyChecking=no -i "/root/.ssh/newpem.pem" ec2-user@13.234.77.141
 docker pull karthidevops1/dev:latest
 docker run -itd -p 80:3000 --name mycont karthidevops1/dev:latest
-
-EOF
 
