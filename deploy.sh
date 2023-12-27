18c4b6971446078b574162effff0234d0a040c21
root <root@ip-172-31-1-215.ap-south-1.compute.internal>
2023-12-27 10:01:52 +0000

    fifth

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-ssh -i "/root/.ssh/newpem.pem" ec2-user@13.234.77.141
+sudo -u jenkins ssh -i "/var/lib/jenkins/.ssh/newpem.pem" ec2-user@13.234.77.141
 docker pull karthidevops1/dev:latest
 docker run -itd -p 80:3000 --name mycont karthidevops1/dev:latest
 
