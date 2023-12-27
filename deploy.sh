2f8016f6c9ec63af3a613c5a62e47f44142ce216
root <root@ip-172-31-1-215.ap-south-1.compute.internal>
2023-12-27 11:33:27 +0000

    final

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 ssh -i "/var/lib/jenkins/.ssh/newpem.pem" ec2-user@13.234.77.141 << EOF
+docker login --username karthidevops1 --password Kandasamy@123
 docker pull karthidevops1/prod:latest
 docker run -itd -p 80:3000 --name mycont4 karthidevops1/prod:latest
 EOF
