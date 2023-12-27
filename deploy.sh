c5317ee7aacc4eb4c839a9ba3fa162c6a540d95d
root <root@ip-172-31-1-215.ap-south-1.compute.internal>
2023-12-27 06:06:33 +0000

    First Commit

---
+++ deploy.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+EC2_INSTANCE_IP="13.234.77.141"
+ssh -i /root/.ssh/newpem.pem ec2-user@$EC2_INSTANCE_IP <<EOF
+docker pull karthidevops1/dev:latest
+docker run -itd -p 80:3000 --name mycont karthidevops1/dev:latest
+
+EOF
+
