54d57db37690d2fdbae85702c327f9e8570038e1
root <root@ip-172-31-1-215.ap-south-1.compute.internal>
2023-12-27 07:32:28 +0000

    Fourth Commit

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 EC2_INSTANCE_IP="13.234.77.141"
-ssh -T -i /root/.ssh/newpem.pem ec2-user@$EC2_INSTANCE_IP <<EOF
+ssh -o StrictHostKeyChecking=no -i /root/.ssh/newpem.pem ec2-user@$EC2_INSTANCE_IP <<EOF
 docker pull karthidevops1/dev:latest
 docker run -itd -p 80:3000 --name mycont karthidevops1/dev:latest
 
